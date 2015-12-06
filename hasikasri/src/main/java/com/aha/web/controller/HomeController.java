0125aaa8d96a8c3eb00f8a3261e3c87c064775c3
vishnukanthmca <vishnukanthmca@gmail.com>
2015-12-06 13:05:09 +0530

    Facebook gradle dependencies working - server starts without dispatcher error

--- hasikasri/src/main/java/com/aha/web/controller/HomeController.java
+++ hasikasri/src/main/java/com/aha/web/controller/HomeController.java
@@ -1,6 +1,7 @@
 package com.aha.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.social.facebook.api.Facebook;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -14,6 +15,9 @@ public class HomeController {
 	private static final String HOME_PAGE = "home";
 	private static final String LISTING_PAGE = "listing";
 	private static final String ERROR_PAGE = "error";
+
+	@Autowired
+	private Facebook faceBook;
 
 	@Autowired
 	private ProductService productService;
